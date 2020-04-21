4415a24e1c03c63b4e5c97b7fc1d2f3b1eef1d08
Jon McManus <due@wxwhatever.com>
2020-04-21 21:12:06 +1000

    Adjust repair recipes.

--- src/main/java/noobanidus/mods/compacted/recipes/RepairRecipe.java
+++ src/main/java/noobanidus/mods/compacted/recipes/RepairRecipe.java
@@ -3,15 +3,21 @@ package noobanidus.mods.compacted.recipes;
 import net.minecraft.inventory.CraftingInventory;
 import net.minecraft.item.IItemTier;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemTier;
 import net.minecraft.item.ToolItem;
 import net.minecraft.item.crafting.IRecipeSerializer;
+import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.item.crafting.SpecialRecipe;
 import net.minecraft.item.crafting.SpecialRecipeSerializer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
+import net.minecraftforge.common.Tags;
+import noobanidus.mods.compacted.CompactedTags;
 import noobanidus.mods.compacted.init.ModRecipes;
 import noobanidus.mods.compacted.items.EffectiveToolItem;
 import noobanidus.mods.compacted.items.PaxelItem;
+import noobanidus.mods.compacted.materials.CompactedStoneMaterial;
+import noobanidus.mods.compacted.materials.DoubleCompactedStoneMaterial;
 
 public class RepairRecipe extends SpecialRecipe {
   public RepairRecipe(ResourceLocation idIn) {
@@ -20,10 +26,13 @@ public class RepairRecipe extends SpecialRecipe {
 
   @Override
   public boolean matches(CraftingInventory inv, World worldIn) {
-    IItemTier tier = null;
     boolean found = false;
     int slot = -1;
     int count = 0;
+    Ingredient tier11 = Ingredient.fromTag(Tags.Items.STONE);
+    Ingredient tier12 = Ingredient.fromTag(Tags.Items.COBBLESTONE);
+    Ingredient tier2 = Ingredient.fromTag(CompactedTags.Items.COMPACTED_COBBLESTONE);
+    Ingredient tier3 = Ingredient.fromTag(CompactedTags.Items.DOUBLE_COMPACTED_COBBLESTONE);
     for (int i = 0; i < inv.getSizeInventory(); i++) {
       ItemStack inSlot = inv.getStackInSlot(i);
       if (inSlot.isEmpty()) {
@@ -36,7 +45,6 @@ public class RepairRecipe extends SpecialRecipe {
         }
 
         found = true;
-        tier = ((ToolItem) inSlot.getItem()).getTier();
         slot = i;
       }
     }
@@ -50,10 +58,19 @@ public class RepairRecipe extends SpecialRecipe {
 
       ItemStack inSlot = inv.getStackInSlot(i);
 
-      if (tier.getRepairMaterial().test(inSlot)) {
-        count++;
+      int thisTier = 0;
+      if (tier11.test(inSlot) || tier12.test(inSlot)) {
+        thisTier = 1;
+      } else if (tier2.test(inSlot)) {
+        thisTier = 2;
+      } else if (tier3.test(inSlot)) {
+        thisTier = 3;
       } else if (!inSlot.isEmpty()) {
         return false;
+      }
+
+      if (thisTier > 0) {
+        count++;
       }
     }
 
@@ -67,16 +84,31 @@ public class RepairRecipe extends SpecialRecipe {
     int slot = -1;
     int count = 0;
 
+    Ingredient tier11 = Ingredient.fromTag(Tags.Items.STONE);
+    Ingredient tier12 = Ingredient.fromTag(Tags.Items.COBBLESTONE);
+    Ingredient tier2 = Ingredient.fromTag(CompactedTags.Items.COMPACTED_COBBLESTONE);
+    Ingredient tier3 = Ingredient.fromTag(CompactedTags.Items.DOUBLE_COMPACTED_COBBLESTONE);
+
+    int tierNum = 0;
+
     for (int i = 0; i < inv.getSizeInventory(); i++) {
       ItemStack inSlot = inv.getStackInSlot(i);
       if (inSlot.isEmpty()) {
         continue;
       }
 
+
       if (inSlot.getItem() instanceof EffectiveToolItem || inSlot.getItem() instanceof PaxelItem) {
         tier = ((ToolItem) inSlot.getItem()).getTier();
         tool = inSlot.copy();
         slot = i;
+        if (tier == ItemTier.STONE) {
+          tierNum = 1;
+        } else if (tier == CompactedStoneMaterial.MATERIAL) {
+          tierNum = 2;
+        } else if (tier == DoubleCompactedStoneMaterial.MATERIAL) {
+          tierNum = 3;
+        }
         break;
       }
     }
@@ -90,15 +122,30 @@ public class RepairRecipe extends SpecialRecipe {
 
       ItemStack inSlot = inv.getStackInSlot(i);
 
-      if (tier.getRepairMaterial().test(inSlot)) {
-        count++;
+      int thisTier = 0;
+      if (tier11.test(inSlot) || tier12.test(inSlot)) {
+        thisTier = 1;
+      } else if (tier2.test(inSlot)) {
+        thisTier = 2;
+      } else if (tier3.test(inSlot)) {
+        thisTier = 3;
+      } else if (!inSlot.isEmpty()) {
+        return ItemStack.EMPTY;
+      }
+
+      if (thisTier == tierNum) {
+        count += 2;
+      } else if (thisTier < tierNum) {
+        count += (tierNum - thisTier);
+      } else {
+        count += 2 + (thisTier - tierNum);
       }
     }
     if (count == 0) {
       return ItemStack.EMPTY;
     }
 
-    int toRepair = (int) Math.floor((((float) tier.getMaxUses()) * 0.1) * count);
+    int toRepair = (int) Math.floor((((float) tier.getMaxUses()) * 0.02) * count);
     tool.setDamage(tool.getDamage() - toRepair);
     return tool;
   }
